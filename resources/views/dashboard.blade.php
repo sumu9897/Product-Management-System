697560ed03bf4c96adc9ea5b6344b6413ca1512d
sumu9897 <mohammad.sumon9897@gmail.com>
2023-10-17 12:13:17 +0600

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,9 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header bg-primary text-white">{{ __('Dashboard') }}</div>
+                    <div class="card-header bg-primary text-white">
+                        <h5>{{ __('Welcome to the Dashboard') }}</h5>
+                    </div>
 
                     <div class="card-body">
                         @if (session('success'))
@@ -14,11 +16,11 @@
                             </div>
                         @endif
 
-                        <h4>Welcome, {{ Auth::user()->name }}!</h4>
+                        <h4 class="mb-4">Greetings, {{ Auth::user()->name }}!</h4>
                         <p class="lead">You are logged in.</p>
 
-                        <div class="content">
-                            <p>Total Products: {{ App\Models\Product::count() }}</p>
+                        <div class="content mt-4">
+                            <p class="h6">Total Products: <span class="badge badge-pill badge-info">{{ App\Models\Product::count() }}</span></p>
                             <!-- Other dashboard content goes here -->
                         </div>
 
